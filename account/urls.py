2b802fe81310f4d14652e9015f08ab2faa1ba9d6
potgieterbt <potgieterbt@gmail.com>
2020-04-10 11:57:55 +0200

    logged-in redirect working
    Signed-off-by: potgieterbt <potgieterbt@gmail.com>

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from django.contrib.auth.decorators import user_passes_test
 
+
+login_forbidden =  user_passes_test(lambda u: u.is_anonymous, '/account/profile')
 
 App_name = 'account'
 urlpatterns = [
     path('', views.account, name='account'),
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('login/', login_forbidden(auth_views.LoginView.as_view(template_name='account/login.html')), name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
 ]
